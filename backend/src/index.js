85aa6f31444caf98eb37b61fec26417ab714eb36
Yogesh Chandra <yogeshchandra52@gmail.com>
2024-12-10 20:42:49 +0530

    chat working

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,62 @@
-require('dotenv').config;
-const express= require('express');
-const {Server}=require('socket.io');
-const http= require('http');
-const cors= require('cors');
-const app = express();
-
-const server = http.createServer(app);
-const io=new Server(server);
-app.use(cors());
-
-
-app.get('/', (req, res) => {
-    res.send('Server is up and running');
-});
-
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const port = 3000;
 
-server.listen(port,() => {console.log(`listening on ${port}`);});
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ _id: "asdasjdhkasdasdas" }, secretKeyJWT);
+
+  res
+    .cookie("token", token, { httpOnly: true, secure: true, sameSite: "none" })
+    .json({
+      message: "Login Success",
+    });
+});
+
+
+io.on("connection", (socket) => {
+  console.log("User Connected", socket.id);
+
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User joined room ${room}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React, { useEffect, useMemo, useState } from "react";
+import { io } from "socket.io-client";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+// const [inputValue,setInutValue] =useState("");
+const [messages, setMessages] = useState("");
+
+  const socket = useMemo(
+    () =>
+      io("http://localhost:3000", {
+        withCredentials: true,
+      }),
+    []
+  );
+
+
+const submitHandler=(e)=>{
+  e.preventDefault();
+  socket.emit("message", messages);
+  setMessages("");
+  
 }
 
-export default App
+
+  useEffect(() => {
+    socket.on("connect", (socket) => {
+      
+      console.log("connected",socket, socket?.id);
+    });
+
+    socket.on("receive-message", (data) => {
+      console.log(data);
+      setMessages((messages) => [...messages, data]);
+    });
+
+    socket.on("welcome", (s) => {
+      console.log(s);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+   <>
+    <h1>HI i am yogesh chandra</h1>
+    <form onSubmit={submitHandler}>
+      <input type="text" onChange={(e)=>setMessages(e.target.value)} value={messages}/>
+      <button type="submit">submit</button>
+    </form>
+   
+   </>
+  );
+};
+
+export default App;
